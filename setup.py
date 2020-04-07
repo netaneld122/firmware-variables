40bc9bfa8187db27b795463b284bc238dcb4dac3
netaneld122 <netaneld122@gmail.com>
2020-04-07 17:34:51 +0300

    Updated setup.py to use setuptools instead of distutils

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='firmware_variables',
-    version='0.0.1-alpha',
+    version='0.0.1',
     description='Windows library for controlling UEFI firmware variables',
-    author='netaneld122@github',
+    author='Netanel Dziubov',
     packages=['firmware_variables'],
     package_dir={'': 'src'},
     install_requires=['pywin32'],
+    url="https://github.com/netaneld122/firmware-variables",
     python_requires='>=3.6',
     classifiers=[
-        'Development Status :: 3 - Alpha',
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
-        'Operating System :: Windows',
+        'Operating System :: Microsoft :: Windows',
     ],
 )
