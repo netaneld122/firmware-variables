98d9063e65166a382f34795f5de134d1612fa86d
Ilay Rosenberg <ilayrosenberg@gmail.com>
2020-04-09 19:13:28 +0300

    Passing keyword arguments to delete_variable (#6)
    * Added `**kwargs` for `delete_variable` to allow calling it with `set_variable`'s keyword arguments

--- src/firmware_variables/variables.py
+++ src/firmware_variables/variables.py
@@ -78,11 +78,11 @@ def set_variable(name, value, namespace=GLOBAL_NAMESPACE, attributes=DEFAULT_ATT
         raise WinError()
 
 
-def delete_variable(name, *args):
+def delete_variable(name, *args, **kwargs):
     """
     Delete the UEFI variable
     :param name: Variable name
     :param namespace: Guid of the form {XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX}
     :param attributes: @see Attributes
     """
-    set_variable(name, value=b"", *args)
+    set_variable(name, value=b"", *args, **kwargs)
