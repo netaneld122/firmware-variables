29f56d3b54c74656385addb999d5d56e0c704015
netaneld122 <netaneld122@gmail.com>
2020-04-10 09:07:21 +0300

    Version -> 0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='firmware_variables',
-    version='0.0.2',
+    version='0.0.3',
     description='Windows library for controlling UEFI firmware variables',
     author='Netanel Dziubov',
     packages=['firmware_variables'],
